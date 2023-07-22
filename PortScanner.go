bfcdb9db453b293c0ffd477cec94afdf38826c26
drakenwolf <hans.haar6@gmail.com>
2023-07-22 18:56:04 -0400

    fix: some errors

--- PortScanner.go
+++ PortScanner.go
@@ -10,11 +10,31 @@ import (
 	"time"
 )
 
+func scanPort(port int, target string) {
+
+	timeout := 1 * time.Second
+
+	conn, err := net.DialTimeout("tcp", target+":"+strconv.Itoa(port), timeout)
+	if err != nil {
+		// fmt.Println("Port", port, "is closed on", target)
+	} else {
+		conn.Close()
+		fmt.Println("Port", port, "is open on", target)
+	}
+
+}
+
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
+	const MAX_PORT int = 65535
+	var ports []int
 
 	for {
+
+		fmt.Print("1. Scan all ports 2. Scan specific ports\nEnter option: ")
+		option, _ := reader.ReadString('\n')
+		option = strings.TrimSpace(option)
 		fmt.Print("Enter target: ")
 		input, _ := reader.ReadString('\n')
 		target := strings.TrimSpace(input)
@@ -23,21 +43,27 @@ func main() {
 			fmt.Println("Exiting")
 			os.Exit(0)
 		}
-
-		ports := []int{80, 443, 8080}
-		timeout := 1 * time.Second
-
-		for _, port := range ports {
-			conn, err := net.DialTimeout("tcp", target+":"+strconv.Itoa(port), timeout)
-			if err != nil {
-				fmt.Println("Port", port, "is closed on", target)
-			} else {
-				conn.Close()
-				fmt.Println("Port", port, "is open on", target)
+		if option == "1" {
+			for port := 0; port < MAX_PORT; port++ {
+				scanPort(port, target)
 			}
-		}
 
+		} else if option == "2" {
+			fmt.Print("Enter ports separated by comma: ")
+			input, _ := reader.ReadString('\n')
+			portslist := strings.Split(strings.TrimSpace(input), ",")
+
+			for _, portstr := range portslist {
+				port, _ := strconv.Atoi(portstr)
+				ports = append(ports, port)
+			}
+			for _, port := range ports {
+				scanPort(port, target)
+			}
+
+		}
 		fmt.Println("Scan completed for", target)
+
 	}
 
 }
